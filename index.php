4dbf312e6f26ea0b32af366bcdc557d819c869f8
sarutn <nattaa.boy@gmail.com>
2024-11-13 10:22:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Asia/Bangkok');
 @ini_set('display_errors', '0');
 
 //$connect = mysqli_connect('sql204.infinityfree.com:3306','if0_36563937','vL6ecbGl4A','if0_36563937_dbmap');
-$connect = mysqli_connect('sql204.infinityfree.com','if0_36563937','vL6ecbGl4A','if0_36563937_dbmap');
+$connect = mysqli_connect('sql204.infinityfree.com:3306','if0_36563937','vL6ecbGl4A','if0_36563937_dbmap');
 mysqli_set_charset($connect,'utf8');
 
 	// Check connection
